be912e70920a4820e90192f23056c7f34d09bd53
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-09-14 21:16:01 -0300

    Realiza requisição em serviço com o axios e construi página Home com listagem dos filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,37 @@
 
+import { useEffect, useState } from 'react';
+import './home.css';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
 export default function Home() {
+  const [filmes, setFilmes] = useState([]);
+  
+  useEffect(()=>{
+
+    async function loadFilmes(){
+        const response = await api.get('r-api/?api=filmes')
+        console.log(response.data);
+        setFilmes(response.data);
+    }
+
+    loadFilmes();
+
+  }, []);
+
   return (
-    <div>
-      <h1>Pagina Home</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme)=>{
+          return(
+            <article key={filme.id}>
+                <strong> {filme.nome} </strong>
+                <img src={filme.foto} alt={filme.nome} />
+                <Link to="/">Acessar</Link>
+            </article> 
+          )
+        })}
+      </div>
     </div>
   );
  }
